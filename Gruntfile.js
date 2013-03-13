89e46b9b5416a9002961ec93ec29f81f787751df
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2013-03-12 23:46:11 -0400

    updating grunt and incr version

--- Gruntfile.js
+++ Gruntfile.js
@@ -275,9 +275,9 @@ module.exports = function (grunt) {
         'uglify:experimental',
         'uglify:joinpatterns',
         'uglify:testing',
-        'uglify:time'//,
+        'uglify:time',
 
-        //'qunit'
+        'qunit'
     ]);    
 
 };
