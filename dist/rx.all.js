569d70060a38d105809bd92525b900ac0c14f2ad
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2014-06-26 13:54:34 -0400

    Adding Observable.of

--- dist/rx.all.js
+++ dist/rx.all.js
@@ -2331,6 +2331,27 @@
         });
     };
 
+  /**
+   *  This method creates a new Observable instance with a variable number of arguments, regardless of number or type of the arguments.
+   * @example
+   *  var res = Rx.Observable.of(1,2,3);
+   * @returns {Observable} The observable sequence whose elements are pulled from the given arguments.
+   */
+  Observable.of = function () {
+    return observableFromArray(arguments);
+  };
+
+  /**
+   *  This method creates a new Observable instance with a variable number of arguments, regardless of number or type of the arguments. 
+   * @example
+   *  var res = Rx.Observable.of(1,2,3);
+   * @param {Scheduler} scheduler A scheduler to use for scheduling the arguments.
+   * @returns {Observable} The observable sequence whose elements are pulled from the given arguments.
+   */
+  Observable.ofWithScheduler = function (scheduler) {
+    return observableFromArray(slice.call(arguments, 1), scheduler);
+  };
+
     /**
      *  Returns a non-terminating observable sequence, which can be used to denote an infinite duration (e.g. when using reactive joins).
      * @returns {Observable} An observable sequence whose observers will never get called.
--- dist/rx.js
+++ dist/rx.js
@@ -2341,6 +2341,27 @@
         });
     };
 
+  /**
+   *  This method creates a new Observable instance with a variable number of arguments, regardless of number or type of the arguments.
+   * @example
+   *  var res = Rx.Observable.of(1,2,3);
+   * @returns {Observable} The observable sequence whose elements are pulled from the given arguments.
+   */
+  Observable.of = function () {
+    return observableFromArray(arguments);
+  };
+
+  /**
+   *  This method creates a new Observable instance with a variable number of arguments, regardless of number or type of the arguments. 
+   * @example
+   *  var res = Rx.Observable.of(1,2,3);
+   * @param {Scheduler} scheduler A scheduler to use for scheduling the arguments.
+   * @returns {Observable} The observable sequence whose elements are pulled from the given arguments.
+   */
+  Observable.ofWithScheduler = function (scheduler) {
+    return observableFromArray(slice.call(arguments, 1), scheduler);
+  };
+
     /**
      *  Generates an observable sequence of integral numbers within a specified range, using the specified scheduler to send out observer messages.
      *  
