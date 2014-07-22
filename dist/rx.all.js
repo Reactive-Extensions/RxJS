df53e183f1b5001846893eb26956fe7f20e7ecbc
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2014-07-22 13:52:14 -0700

    Fixing whitespace

--- dist/rx.all.js
+++ dist/rx.all.js
@@ -7052,47 +7052,47 @@
         return new Plan(this, selector);
     };
 
-    function Plan(expression, selector) {
-        this.expression = expression;
-        this.selector = selector;
+  function Plan(expression, selector) {
+      this.expression = expression;
+      this.selector = selector;
+  }
+  
+  Plan.prototype.activate = function (externalSubscriptions, observer, deactivate) {
+    var self = this;
+    var joinObservers = [];
+    for (var i = 0, len = this.expression.patterns.length; i < len; i++) {
+      joinObservers.push(planCreateObserver(externalSubscriptions, this.expression.patterns[i], observer.onError.bind(observer)));
     }
-    
-    Plan.prototype.activate = function (externalSubscriptions, observer, deactivate) {
-        var self = this;
-        var joinObservers = [];
-        for (var i = 0, len = this.expression.patterns.length; i < len; i++) {
-            joinObservers.push(planCreateObserver(externalSubscriptions, this.expression.patterns[i], observer.onError.bind(observer)));
-        }
-        var activePlan = new ActivePlan(joinObservers, function () {
-            var result;
-            try {
-                result = self.selector.apply(self, arguments);
-            } catch (exception) {
-                observer.onError(exception);
-                return;
-            }
-            observer.onNext(result);
-        }, function () {
-            for (var j = 0, jlen = joinObservers.length; j < jlen; j++) {
-                joinObservers[j].removeActivePlan(activePlan);
-            }
-            deactivate(activePlan);
-        });
-        for (i = 0, len = joinObservers.length; i < len; i++) {
-            joinObservers[i].addActivePlan(activePlan);
-        }
-        return activePlan;
-    };
+    var activePlan = new ActivePlan(joinObservers, function () {
+      var result;
+      try {
+        result = self.selector.apply(self, arguments);
+      } catch (exception) {
+        observer.onError(exception);
+        return;
+      }
+      observer.onNext(result);
+    }, function () {
+      for (var j = 0, jlen = joinObservers.length; j < jlen; j++) {
+        joinObservers[j].removeActivePlan(activePlan);
+      }
+      deactivate(activePlan);
+    });
+    for (i = 0, len = joinObservers.length; i < len; i++) {
+      joinObservers[i].addActivePlan(activePlan);
+    }
+    return activePlan;
+  };
 
-    function planCreateObserver(externalSubscriptions, observable, onError) {
-        var entry = externalSubscriptions.get(observable);
-        if (!entry) {
-            var observer = new JoinObserver(observable, onError);
-            externalSubscriptions.set(observable, observer);
-            return observer;
-        }
-        return entry;
+  function planCreateObserver(externalSubscriptions, observable, onError) {
+    var entry = externalSubscriptions.get(observable);
+    if (!entry) {
+      var observer = new JoinObserver(observable, onError);
+      externalSubscriptions.set(observable, observer);
+      return observer;
     }
+    return entry;
+  }
 
     // Active Plan
     function ActivePlan(joinObserverArray, onNext, onCompleted) {
@@ -7237,25 +7237,25 @@
         return JoinObserver;
     } (AbstractObserver));
 
-    /**
-     *  Creates a pattern that matches when both observable sequences have an available value.
-     *  
-     *  @param right Observable sequence to match with the current sequence.
-     *  @return {Pattern} Pattern object that matches when both observable sequences have an available value.     
-     */
-    observableProto.and = function (right) {
-        return new Pattern([this, right]);
-    };
+  /**
+   *  Creates a pattern that matches when both observable sequences have an available value.
+   *  
+   *  @param right Observable sequence to match with the current sequence.
+   *  @return {Pattern} Pattern object that matches when both observable sequences have an available value.     
+   */
+  observableProto.and = function (right) {
+    return new Pattern([this, right]);
+  };
 
-    /**
-     *  Matches when the observable sequence has an available value and projects the value.
-     *  
-     *  @param selector Selector that will be invoked for values in the source sequence.
-     *  @returns {Plan} Plan that produces the projected values, to be fed (with other plans) to the when operator. 
-     */    
-    observableProto.thenDo = function (selector) {
-        return new Pattern([this]).thenDo(selector);
-    };
+  /**
+   *  Matches when the observable sequence has an available value and projects the value.
+   *  
+   *  @param selector Selector that will be invoked for values in the source sequence.
+   *  @returns {Plan} Plan that produces the projected values, to be fed (with other plans) to the when operator. 
+   */    
+  observableProto.thenDo = function (selector) {
+    return new Pattern([this]).thenDo(selector);
+  };
 
     /**
      *  Joins together the results from several patterns.
