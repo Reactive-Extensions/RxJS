b62526bdff19d6ce24dd4ee666dbd0ada6ea1e42
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2014-09-23 17:46:43 -0400

    Fixing Issue #283

--- src/core/linq/observable/windowwithtime.js
+++ src/core/linq/observable/windowwithtime.js
@@ -12,70 +12,49 @@
    */
   observableProto.windowWithTime = function (timeSpan, timeShiftOrScheduler, scheduler) {
     var source = this, timeShift;
-
-    timeShiftOrScheduler == null && (timeShift = timeSpan);
+    if (timeShiftOrScheduler === undefined) {
+      timeShift = timeSpan;
+    }
     isScheduler(scheduler) || (scheduler = timeoutScheduler);
     if (typeof timeShiftOrScheduler === 'number') {
       timeShift = timeShiftOrScheduler;
-    } else if (typeof timeShiftOrScheduler === 'object') {
+    } else if (isScheduler(timeShiftOrScheduler)) {
       timeShift = timeSpan;
       scheduler = timeShiftOrScheduler;
     }
     return new AnonymousObservable(function (observer) {
       var groupDisposable,
-        nextShift = timeShift,
-        nextSpan = timeSpan,
-        q = [],
-        refCountDisposable,
-        timerD = new SerialDisposable(),
-        totalTime = 0;
-        groupDisposable = new CompositeDisposable(timerD),
-        refCountDisposable = new RefCountDisposable(groupDisposable);
+          nextShift = timeShift,
+          nextSpan = timeSpan,
+          q = [],
+          refCountDisposable,
+          timerD = new SerialDisposable(),
+          totalTime = 0;
+          groupDisposable = new CompositeDisposable(timerD),
+          refCountDisposable = new RefCountDisposable(groupDisposable);
 
-
-      q.push(new Subject());
-      observer.onNext(addRef(q[0], refCountDisposable));
-      createTimer();
-      groupDisposable.add(source.subscribe(function (x) {
-        var i, len;
-        for (i = 0, len = q.length; i < len; i++) {
-          q[i].onNext(x);
-        }
-      }, function (e) {
-        var i, len;
-        for (i = 0, len = q.length; i < len; i++) {
-          q[i].onError(e);
-        }
-        observer.onError(e);
-      }, function () {
-        var i, len;
-        for (i = 0, len = q.length; i < len; i++) {
-          q[i].onCompleted();
-        }
-        observer.onCompleted();
-      }));
-
-      return refCountDisposable;
-
-      function createTimer () {
-        var m = new SingleAssignmentDisposable(), isSpan = false, isShift = false;
+       function createTimer () {
+        var m = new SingleAssignmentDisposable(),
+            isSpan = false,
+            isShift = false;
         timerD.setDisposable(m);
-
         if (nextSpan === nextShift) {
           isSpan = true;
           isShift = true;
         } else if (nextSpan < nextShift) {
-          isSpan = true;
+            isSpan = true;
         } else {
           isShift = true;
         }
-
         var newTotalTime = isSpan ? nextSpan : nextShift,
-          ts = newTotalTime - totalTime;
+            ts = newTotalTime - totalTime;
         totalTime = newTotalTime;
-        isSpan && (nextSpan += timeShift);
-        isShift && (nextShift += timeShift);
-
+        if (isSpan) {
+          nextSpan += timeShift;
+        }
+        if (isShift) {
+          nextShift += timeShift;
+        }
         m.setDisposable(scheduler.scheduleWithRelative(ts, function () {
           var s;
           if (isShift) {
@@ -89,6 +68,25 @@
           }
           createTimer();
         }));
-      }      
+      };
+      q.push(new Subject());
+      observer.onNext(addRef(q[0], refCountDisposable));
+      createTimer();
+      groupDisposable.add(source.subscribe(function (x) {
+        for (var i = 0, len = q.length; i < len; i++) {
+          q[i].onNext(x);
+        }
+      }, function (e) {
+        for (var i = 0, len = q.length; i < len; i++) {
+          q[i].onError(e);
+        }
+        observer.onError(e);
+      }, function () {
+        for (var i = 0, len = q.length; i < len; i++) {
+          q[i].onCompleted();
+        }
+        observer.onCompleted();
+      }));
+      return refCountDisposable;
     });
   };
