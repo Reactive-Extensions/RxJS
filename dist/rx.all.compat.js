813c3516c2be050a790c700ed1cccb27931697ba
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2014-11-30 05:25:19 +0100

    Closing Issue #394

--- dist/rx.all.compat.js
+++ dist/rx.all.compat.js
@@ -1189,15 +1189,13 @@ if (!Array.prototype.forEach) {
     return SchedulePeriodicRecursive;
   }());
 
-  /**
-   * Gets a scheduler that schedules work immediately on the current thread.
-   */
+  /** Gets a scheduler that schedules work immediately on the current thread. */
   var immediateScheduler = Scheduler.immediate = (function () {
 
     function scheduleNow(state, action) { return action(this, state); }
 
     function scheduleRelative(state, dueTime, action) {
-      var dt = normalizeTime(dt);
+      var dt = normalizeTime(dueTime);
       while (dt - this.now() > 0) { }
       return action(this, state);
     }
