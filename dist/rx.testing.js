da1e7e219bb0e57eb59670d62e7757fee58e330d
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2014-12-01 15:52:50 -0500

    Fixing Issue #416

--- dist/rx.testing.js
+++ dist/rx.testing.js
@@ -238,17 +238,17 @@ var ReactiveTest = Rx.ReactiveTest = {
   })(Observer);
 
   function MockPromise(scheduler, messages) {
-    var message, notification, observable = this;
+    var self = this;
     this.scheduler = scheduler;
     this.messages = messages;
     this.subscriptions = [];
     this.observers = [];
     for (var i = 0, len = this.messages.length; i < len; i++) {
-      message = this.messages[i];
-      notification = message.value;
+      var message = this.messages[i],
+          notification = message.value;
       (function (innerNotification) {
         scheduler.scheduleAbsoluteWithState(null, message.time, function () {
-          var obs = observable.observers.slice(0);
+          var obs = self.observers.slice(0);
 
           for (var j = 0, jLen = obs.length; j < jLen; j++) {
             innerNotification.accept(obs[j]);
@@ -265,15 +265,23 @@ var ReactiveTest = Rx.ReactiveTest = {
     this.subscriptions.push(new Subscription(this.scheduler.clock));
     var index = this.subscriptions.length - 1;
 
+    var newPromise;
+
     var observer = Rx.Observer.create(
       function (x) {
-        self.messages = onResolved(x) || [];
+        var retValue = onResolved(x);
+        if (retValue && typeof retValue.then === 'function') {
+          newPromise = retValue;
+        } else {
+          var ticks = self.scheduler.clock;
+          newPromise = new MockPromise(self.scheduler, [Rx.ReactiveTest.onNext(ticks, undefined), Rx.ReactiveTest.onCompleted(ticks)]);
+        }
         var idx = self.observers.indexOf(observer);
         self.observers.splice(idx, 1);
         self.subscriptions[index] = new Subscription(self.subscriptions[index].subscribe, self.scheduler.clock);
       },
       function (err) {
-        self.messages = onRejected(err) || [];
+        onRejected(err);
         var idx = self.observers.indexOf(observer);
         self.observers.splice(idx, 1);
         self.subscriptions[index] = new Subscription(self.subscriptions[index].subscribe, self.scheduler.clock);
@@ -281,7 +289,7 @@ var ReactiveTest = Rx.ReactiveTest = {
     );
     this.observers.push(observer);
 
-    return new MockPromise(this.scheduler, this.messages);
+    return newPromise || new MockPromise(this.scheduler, this.messages);
   };
 
   var HotObservable = (function (__super__) {
--- src/core/testing/mockpromise.js
+++ src/core/testing/mockpromise.js
@@ -1,15 +1,15 @@
   function MockPromise(scheduler, messages) {
-    var message, notification, observable = this;
+    var self = this;
     this.scheduler = scheduler;
     this.messages = messages;
     this.subscriptions = [];
     this.observers = [];
     for (var i = 0, len = this.messages.length; i < len; i++) {
-      message = this.messages[i];
-      notification = message.value;
+      var message = this.messages[i],
+          notification = message.value;
       (function (innerNotification) {
         scheduler.scheduleAbsoluteWithState(null, message.time, function () {
-          var obs = observable.observers.slice(0);
+          var obs = self.observers.slice(0);
 
           for (var j = 0, jLen = obs.length; j < jLen; j++) {
             innerNotification.accept(obs[j]);
@@ -26,15 +26,23 @@
     this.subscriptions.push(new Subscription(this.scheduler.clock));
     var index = this.subscriptions.length - 1;
 
+    var newPromise;
+
     var observer = Rx.Observer.create(
       function (x) {
-        self.messages = onResolved(x) || [];
+        var retValue = onResolved(x);
+        if (retValue && typeof retValue.then === 'function') {
+          newPromise = retValue;
+        } else {
+          var ticks = self.scheduler.clock;
+          newPromise = new MockPromise(self.scheduler, [Rx.ReactiveTest.onNext(ticks, undefined), Rx.ReactiveTest.onCompleted(ticks)]);
+        }
         var idx = self.observers.indexOf(observer);
         self.observers.splice(idx, 1);
         self.subscriptions[index] = new Subscription(self.subscriptions[index].subscribe, self.scheduler.clock);
       },
       function (err) {
-        self.messages = onRejected(err) || [];
+        onRejected(err);
         var idx = self.observers.indexOf(observer);
         self.observers.splice(idx, 1);
         self.subscriptions[index] = new Subscription(self.subscriptions[index].subscribe, self.scheduler.clock);
@@ -42,5 +50,5 @@
     );
     this.observers.push(observer);
 
-    return new MockPromise(this.scheduler, this.messages);
+    return newPromise || new MockPromise(this.scheduler, this.messages);
   };
