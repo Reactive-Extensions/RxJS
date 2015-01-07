9004566df31b94469af1da66e0854b450ea4a714
Ben Lesh <blesh@netflix.com>
2015-01-07 14:31:17 -0800

    add additional test for next then complete

--- tests/observable/retrywhen.js
+++ tests/observable/retrywhen.js
@@ -23,7 +23,7 @@ test('RetryWhen_Observable_Cold_Basic', function () {
     xs.subscriptions.assertEqual(subscribe(200, 450));
 });
 
-test('RetryWhen_Observable_Cold_Error', function () {
+test('RetryWhen_Observable_Cold_Error_Continue', function () {
     var results, scheduler, xs, retry, ex;
     ex = 'ex';
     scheduler = new TestScheduler();
@@ -36,3 +36,31 @@ test('RetryWhen_Observable_Cold_Error', function () {
     xs.subscriptions.assertEqual(subscribe(200, 240), subscribe(240, 1000));
 });
 
+
+test('RetryWhen_Observable_Cold_Error_Complete', function () {
+    var results, scheduler, xs, retry, ex;
+    ex = 'ex';
+    scheduler = new TestScheduler();
+    retry = scheduler.createHotObservable(onCompleted(240));
+    xs = scheduler.createColdObservable(onNext(10, 1), onNext(20, 2), onError(30, ex), onCompleted(40));
+    results = scheduler.startWithCreate(function () {
+        return xs.retryWhen(retry);
+    });
+    results.messages.assertEqual(onNext(210, 1), onNext(220, 2), onCompleted(240));
+    xs.subscriptions.assertEqual(subscribe(200, 240));
+});
+
+
+
+test('RetryWhen_Observable_Cold_Error_Next_Complete', function () {
+    var results, scheduler, xs, retry, ex;
+    ex = 'ex';
+    scheduler = new TestScheduler();
+    retry = scheduler.createHotObservable(onNext(240), onCompleted(300));
+    xs = scheduler.createColdObservable(onNext(10, 1), onNext(20, 2), onError(30, ex), onCompleted(40));
+    results = scheduler.startWithCreate(function () {
+        return xs.retryWhen(retry);
+    });
+    results.messages.assertEqual(onNext(210, 1), onNext(220, 2), onNext(250, 1), onNext(260, 2), onCompleted(300));
+    xs.subscriptions.assertEqual(subscribe(200, 240), subscribe(240, 300));
+});
