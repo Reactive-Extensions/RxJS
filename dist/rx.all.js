ad356e903477781179b3d23e777713418804f317
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2015-04-26 18:13:01 -0300

    Fixing switch perf

--- dist/rx.all.js
+++ dist/rx.all.js
@@ -3729,43 +3729,100 @@
     }, source);
   };
 
+  var SwitchObservable = (function(__super__) {
+    inherits(SwitchObservable, __super__);
+    function SwitchObservable(source) {
+      this.source = source;
+      __super__.call(this);
+    }
+
+    SwitchObservable.prototype.subscribeCore = function (o) {
+      var inner = new SerialDisposable(), s = this.source.subscribe(new SwitchObserver(o, inner));
+      return new CompositeDisposable(s, inner);
+    };
+
+    function SwitchObserver(o, inner) {
+      this.o = o;
+      this.inner = inner;
+      this.stopped = false;
+      this.latest = 0;
+      this.hasLatest = false;
+      this.isStopped = false;
+    }
+    SwitchObserver.prototype.onNext = function (innerSource) {
+      if (this.isStopped) { return; }
+      var d = new SingleAssignmentDisposable(), id = ++this.latest;
+      this.hasLatest = true;
+      this.inner.setDisposable(d);
+      isPromise(innerSource) && (innerSource = observableFromPromise(innerSource));
+      d.setDisposable(innerSource.subscribe(new InnerObserver(this, id)));
+    };
+    SwitchObserver.prototype.onError = function (e) {
+      if (!this.isStopped) {
+        this.isStopped = true;
+        this.o.onError(e);
+      }
+    };
+    SwitchObserver.prototype.onCompleted = function () {
+      if (!this.isStopped) {
+        this.isStopped = true;
+        this.stopped = true;
+        !this.hasLatest && this.o.onCompleted();
+      }
+    };
+    SwitchObserver.prototype.dispose = function () { this.isStopped = true; };
+    SwitchObserver.prototype.fail = function (e) {
+      if(!this.isStopped) {
+        this.isStopped = true;
+        this.o.onError(e);
+        return true;
+      }
+      return false;
+    };
+
+    function InnerObserver(parent, id) {
+      this.parent = parent;
+      this.id = id;
+      this.isStopped = false;
+    }
+    InnerObserver.prototype.onNext = function (x) {
+      if (this.isStopped) { return; }
+      this.parent.latest === this.id && this.parent.o.onNext(x);
+    };
+    InnerObserver.prototype.onError = function (e) {
+      if (!this.isStopped) {
+        this.isStopped = true;
+        this.parent.latest === this.id && this.parent.o.onError(e);
+      }
+    };
+    InnerObserver.prototype.onCompleted = function () {
+      if (!this.isStopped) {
+        this.isStopped = true;
+        if (this.parent.latest === this.id) {
+          this.parent.hasLatest = false;
+          this.parent.isStopped && this.parent.o.onCompleted();
+        }
+      }
+    };
+    InnerObserver.prototype.dispose = function () { this.isStopped = true; }
+    InnerObserver.prototype.fail = function (e) {
+      if(!this.isStopped) {
+        this.isStopped = true;
+        this.parent.o.onError(e);
+        return true;
+      }
+      return false;
+    };
+
+    return SwitchObservable;
+  }(ObservableBase));
+
   /**
-   * Transforms an observable sequence of observable sequences into an observable sequence producing values only from the most recent observable sequence.
-   * @returns {Observable} The observable sequence that at any point in time produces the elements of the most recent inner observable sequence that has been received.
-   */
+  * Transforms an observable sequence of observable sequences into an observable sequence producing values only from the most recent observable sequence.
+  * @returns {Observable} The observable sequence that at any point in time produces the elements of the most recent inner observable sequence that has been received.
+  */
   observableProto['switch'] = observableProto.switchLatest = function () {
-    var sources = this;
-    return new AnonymousObservable(function (observer) {
-      var hasLatest = false,
-        innerSubscription = new SerialDisposable(),
-        isStopped = false,
-        latest = 0,
-        subscription = sources.subscribe(
-          function (innerSource) {
-            var d = new SingleAssignmentDisposable(), id = ++latest;
-            hasLatest = true;
-            innerSubscription.setDisposable(d);
-
-            // Check if Promise or Observable
-            isPromise(innerSource) && (innerSource = observableFromPromise(innerSource));
-
-            d.setDisposable(innerSource.subscribe(
-              function (x) { latest === id && observer.onNext(x); },
-              function (e) { latest === id && observer.onError(e); },
-              function () {
-                if (latest === id) {
-                  hasLatest = false;
-                  isStopped && observer.onCompleted();
-                }
-              }));
-          },
-          function (e) { observer.onError(e); },
-          function () {
-            isStopped = true;
-            !hasLatest && observer.onCompleted();
-          });
-      return new CompositeDisposable(subscription, innerSubscription);
-    }, sources);
+    return new SwitchObservable(this);
   };
 
   var TakeUntilObservable = (function(__super__) {
--- src/core/perf/operators/switch.js
+++ src/core/perf/operators/switch.js
@@ -5,66 +5,86 @@
       __super__.call(this);
     }
 
-    SwitchObservable.prototype.subscribeCore = function (observer) {
-      var innerSubscription = new SerialDisposable(),
-        subscription = this.source.subscribe(new SwitchObserver(observer, innerSubscription));
-      return new CompositeDisposable(subscription, innerSubscription);
+    SwitchObservable.prototype.subscribeCore = function (o) {
+      var inner = new SerialDisposable(), s = this.source.subscribe(new SwitchObserver(o, inner));
+      return new CompositeDisposable(s, inner);
+    };
+
+    function SwitchObserver(o, inner) {
+      this.o = o;
+      this.inner = inner;
+      this.stopped = false;
+      this.latest = 0;
+      this.hasLatest = false;
+      this.isStopped = false;
+    }
+    SwitchObserver.prototype.onNext = function (innerSource) {
+      if (this.isStopped) { return; }
+      var d = new SingleAssignmentDisposable(), id = ++this.latest;
+      this.hasLatest = true;
+      this.inner.setDisposable(d);
+      isPromise(innerSource) && (innerSource = observableFromPromise(innerSource));
+      d.setDisposable(innerSource.subscribe(new InnerObserver(this, id)));
+    };
+    SwitchObserver.prototype.onError = function (e) {
+      if (!this.isStopped) {
+        this.isStopped = true;
+        this.o.onError(e);
+      }
+    };
+    SwitchObserver.prototype.onCompleted = function () {
+      if (!this.isStopped) {
+        this.isStopped = true;
+        this.stopped = true;
+        !this.hasLatest && this.o.onCompleted();
+      }
+    };
+    SwitchObserver.prototype.dispose = function () { this.isStopped = true; };
+    SwitchObserver.prototype.fail = function (e) {
+      if(!this.isStopped) {
+        this.isStopped = true;
+        this.o.onError(e);
+        return true;
+      }
+      return false;
+    };
+
+    function InnerObserver(parent, id) {
+      this.parent = parent;
+      this.id = id;
+      this.isStopped = false;
+    }
+    InnerObserver.prototype.onNext = function (x) {
+      if (this.isStopped) { return; }
+      this.parent.latest === this.id && this.parent.o.onNext(x);
+    };
+    InnerObserver.prototype.onError = function (e) {
+      if (!this.isStopped) {
+        this.isStopped = true;
+        this.parent.latest === this.id && this.parent.o.onError(e);
+      }
+    };
+    InnerObserver.prototype.onCompleted = function () {
+      if (!this.isStopped) {
+        this.isStopped = true;
+        if (this.parent.latest === this.id) {
+          this.parent.hasLatest = false;
+          this.parent.isStopped && this.parent.o.onCompleted();
+        }
+      }
+    };
+    InnerObserver.prototype.dispose = function () { this.isStopped = true; }
+    InnerObserver.prototype.fail = function (e) {
+      if(!this.isStopped) {
+        this.isStopped = true;
+        this.parent.o.onError(e);
+        return true;
+      }
+      return false;
     };
 
     return SwitchObservable;
   }(ObservableBase));
-
-  var SwitchObserver = (function(__super__) {
-    inherits(SwitchObserver, __super__);
-    function SwitchObserver(observer, innerSubscription) {
-      this.observer = observer;
-      this.innerSubscription = innerSubscription;
-      this.stopped = false;
-      this.latest = 0;
-      this.hasLatest = false;
-      __super__.call(this);
-    }
-
-    SwitchObserver.prototype.next = function (innerSource) {
-      var d = new SingleAssignmentDisposable(), id = ++this.latest;
-      this.hasLatest = true;
-      this.innerSubscription.setDisposable(d);
-
-      isPromise(innerSource) && (innerSource = observableFromPromise(innerSource));
-
-      d.setDisposable(innerSource.subscribe(new InnerObserver(this, id)));
-    };
-
-    SwitchObserver.prototype.error = function (e) {
-      this.observer.onError(e);
-    };
-
-    SwitchObserver.prototype.completed = function () {
-      this.stopped = true;
-      !this.hasLatest && this.observer.onCompleted();
-    };
-
-    var InnerObserver = (function(__base__) {
-      inherits(InnerObserver, __base__);
-      function InnerObserver(parent, id) {
-        this.parent = parent;
-        this.id = id;
-        __base__.call(this);
-      }
-      InnerObserver.prototype.next = function (x) { this.parent.latest === this.id && this.parent.observer.onNext(x); };
-      InnerObserver.prototype.error = function (e) { this.parent.latest === this.id && this.parent.observer.onError(e); };
-      InnerObserver.prototype.completed = function () {
-        if (this.parent.latest === this.id) {
-          this.parent.hasLatest = false;
-          this.parent.isStopped && this.parent.observer.onCompleted();
-        }
-      };
-
-      return InnerObserver;
-    }(AbstractObserver));
-
-    return SwitchObserver;
-  }(AbstractObserver));
 
   /**
   * Transforms an observable sequence of observable sequences into an observable sequence producing values only from the most recent observable sequence.
