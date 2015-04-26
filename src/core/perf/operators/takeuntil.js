1a22f12becaf19b21f59962ba4da292cfb83e62a
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2015-04-26 10:18:07 -0300

    Fixing spacing

--- src/core/perf/operators/takeuntil.js
+++ src/core/perf/operators/takeuntil.js
@@ -1,50 +1,50 @@
   var TakeUntilObservable = (function(__super__) {
-		inherits(TakeUntilObservable, __super__);
-		
-		function TakeUntilObservable(source, other) {
-			this.source = source;
-			this.other = isPromise(other) ? observableFromPromise(other) : other;
-			__super__.call(this);
-		}
-		
-		TakeUntilObservable.prototype.subscribeCore = function(o) {
-			return new CompositeDisposable(
-				this.source.subscribe(o),
-				this.other.subscribe(new InnerObserver(o))
-			);
-		};
-		
-		function InnerObserver(o) {
-			this.o = o;
-			this.isStopped = false;
-		}
-		InnerObserver.prototype.onNext = function (x) {
-			if (this.isStopped) { return; }
-			this.o.onCompleted();
-		};
-		InnerObserver.prototype.onError = function (err) {
-			if (!this.isStopped) {
-				this.isStopped = true;
-				this.o.onError(err);
-			}
-		};
-		InnerObserver.prototype.onCompleted = function () {
-			!this.isStopped && (this.isStopped = true);
-		};
-	  InnerObserver.prototype.dispose = function() { this.isStopped = true; };
-	  InnerObserver.prototype.fail = function (e) {
-	    if (!this.isStopped) {
-	      this.isStopped = true;
-	      this.observer.onError(e);
-	      return true;
-	    }
-	    return false;
-	  };
+    inherits(TakeUntilObservable, __super__);
 
-		return TakeUntilObservable;
-	}(ObservableBase));
-	
-	/**
+    function TakeUntilObservable(source, other) {
+      this.source = source;
+      this.other = isPromise(other) ? observableFromPromise(other) : other;
+      __super__.call(this);
+    }
+
+    TakeUntilObservable.prototype.subscribeCore = function(o) {
+      return new CompositeDisposable(
+        this.source.subscribe(o),
+        this.other.subscribe(new InnerObserver(o))
+      );
+    };
+
+    function InnerObserver(o) {
+      this.o = o;
+      this.isStopped = false;
+    }
+    InnerObserver.prototype.onNext = function (x) {
+      if (this.isStopped) { return; }
+      this.o.onCompleted();
+    };
+    InnerObserver.prototype.onError = function (err) {
+      if (!this.isStopped) {
+        this.isStopped = true;
+        this.o.onError(err);
+      }
+    };
+    InnerObserver.prototype.onCompleted = function () {
+      !this.isStopped && (this.isStopped = true);
+    };
+    InnerObserver.prototype.dispose = function() { this.isStopped = true; };
+    InnerObserver.prototype.fail = function (e) {
+      if (!this.isStopped) {
+        this.isStopped = true;
+        this.observer.onError(e);
+        return true;
+      }
+      return false;
+    };
+
+    return TakeUntilObservable;
+  }(ObservableBase));
+
+  /**
    * Returns the values from the source observable sequence until the other observable sequence produces a value.
    * @param {Observable | Promise} other Observable sequence or Promise that terminates propagation of elements of the source sequence.
    * @returns {Observable} An observable sequence containing the elements of the source sequence up to the point the other sequence interrupted further propagation.
