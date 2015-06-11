a14dcb76f53fba64df4ed395dc3ee6c72d3b0761
Kent Wong <kent.wong@autodesk.com>
2015-06-11 13:31:36 -0400

    Fixed the return type.

--- ts/rx-lite.d.ts
+++ ts/rx-lite.d.ts
@@ -354,7 +354,7 @@ declare module Rx {
 		 * @param {Any} [thisArg] An optional "this" to use to invoke each transform.
 		 * @returns {Observable} An observable sequence whose elements are the result of invoking the one-to-many transform function corresponding to each notification in the input sequence.
 		 */
-		selectManyObserver<T2, T3, T4, R>(onNext: (value: T, index: number) => Observable<T2>, onError: (exception: any) => Observable<T3>, onCompleted: () => Observable<T4>, thisArg?: any): Observable<R>;
+		selectManyObserver<T2, T3, T4>(onNext: (value: T, index: number) => Observable<T2>, onError: (exception: any) => Observable<T3>, onCompleted: () => Observable<T4>, thisArg?: any): Observable<T2 | T3 | T4>;
 
 		/**
 		 * Projects each notification of an observable sequence to an observable sequence and merges the resulting observable sequences into one observable sequence.
@@ -364,7 +364,7 @@ declare module Rx {
 		 * @param {Any} [thisArg] An optional "this" to use to invoke each transform.
 		 * @returns {Observable} An observable sequence whose elements are the result of invoking the one-to-many transform function corresponding to each notification in the input sequence.
 		 */
-		flatMapObserver<T2, T3, T4, R>(onNext: (value: T, index: number) => Observable<T2>, onError: (exception: any) => Observable<T3>, onCompleted: () => Observable<T4>, thisArg?: any): Observable<R>;
+		flatMapObserver<T2, T3, T4>(onNext: (value: T, index: number) => Observable<T2>, onError: (exception: any) => Observable<T3>, onCompleted: () => Observable<T4>, thisArg?: any): Observable<T2 | T3 | T4>;
 
 		selectConcat<T2, R>(selector: (value: T, index: number) => Observable<T2>, resultSelector: (value1: T, value2: T2, index: number) => R): Observable<R>;
 		selectConcat<T2, R>(selector: (value: T, index: number) => IPromise<T2>, resultSelector: (value1: T, value2: T2, index: number) => R): Observable<R>;
