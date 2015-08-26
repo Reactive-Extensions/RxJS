791280bc9fb3db154edecd762e1a912678799f2c
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2015-08-26 13:31:11 -0400

    Modifying tests

--- tests/observable/debounce.js
+++ tests/observable/debounce.js
@@ -74,24 +74,25 @@
     );
   });
 
-  test('debounce All Pass', function(){
+  test('debounce all pass', function(){
     var scheduler = new TestScheduler();
+
     var xs = scheduler.createHotObservable(
-        onNext(210, 1),
-        onNext(300, 2),
-        onNext(351, 3),
-        onCompleted(400)
+      onNext(210, 1),
+      onNext(300, 2),
+      onNext(351, 3),
+      onCompleted(400)
     );
 
     var results = scheduler.startScheduler(function(){
-        return xs.debounce(50, scheduler);
+      return xs.debounce(50, scheduler);
     });
 
     results.messages.assertEqual(
-        onNext(260, 1),
-        onNext(350, 2),
-        onNext(400, 3),
-        onCompleted(400)
+      onNext(260, 1),
+      onNext(350, 2),
+      onNext(400, 3),
+      onCompleted(400)
     );
   });
 
@@ -124,7 +125,7 @@
       );
   });
 
-  test('debounce TimeSpan AllPass ErrorEnd', function () {
+  test('debounce relative time AllPass ErrorEnd', function () {
       var error = new Error();
 
       var scheduler = new TestScheduler();
@@ -155,32 +156,32 @@
       );
   });
 
-  test('debounce TimeSpan AllDrop', function () {
+  test('debounce relative time all drop', function () {
       var scheduler = new TestScheduler();
 
       var xs = scheduler.createHotObservable(
-          onNext(150, 0),
-          onNext(210, 1),
-          onNext(240, 2),
-          onNext(270, 3),
-          onNext(300, 4),
-          onNext(330, 5),
-          onNext(360, 6),
-          onNext(390, 7),
-          onCompleted(400)
+        onNext(150, 0),
+        onNext(210, 1),
+        onNext(240, 2),
+        onNext(270, 3),
+        onNext(300, 4),
+        onNext(330, 5),
+        onNext(360, 6),
+        onNext(390, 7),
+        onCompleted(400)
       );
 
       var res = scheduler.startScheduler(function () {
-          return xs.debounce(function () { return Observable.timer(40, scheduler); });
+        return xs.debounce(function () { return Observable.timer(40, scheduler); });
       });
 
       res.messages.assertEqual(
-          onNext(400, 7),
-          onCompleted(400)
+        onNext(400, 7),
+        onCompleted(400)
       );
 
       xs.subscriptions.assertEqual(
-          subscribe(200, 400)
+        subscribe(200, 400)
       );
   });
 
@@ -202,7 +203,7 @@
     );
 
     var res = scheduler.startScheduler(function () {
-      return xs.debounce(40, scheduler)
+      return xs.debounce(40, scheduler);
     });
 
     res.messages.assertEqual(
@@ -214,7 +215,7 @@
     );
   });
 
-  test('debounce Duration DelayBehavior', function () {
+  test('debounce duration DelayBehavior', function () {
     var scheduler = new TestScheduler();
 
     var xs = scheduler.createHotObservable(
@@ -236,7 +237,7 @@
     ];
 
     var results = scheduler.startScheduler(function () {
-      return xs.debounce((function (x) { return ys[x]; }));
+      return xs.debounce(function (x) { return ys[x]; });
     });
 
     results.messages.assertEqual(
@@ -256,70 +257,139 @@
     ys[4].subscriptions.assertEqual(subscribe(400, 400 + 20));
   });
 
-  test('debounce Duration debounceBehavior', function () {
-      var scheduler = new TestScheduler();
-      var xs = scheduler.createHotObservable(onNext(150, -1), onNext(250, 0), onNext(280, 1), onNext(310, 2), onNext(350, 3), onNext(400, 4), onCompleted(550));
-      var ys = [scheduler.createColdObservable(onNext(20, 42), onNext(25, 99)), scheduler.createColdObservable(onNext(40, 42), onNext(45, 99)), scheduler.createColdObservable(onNext(20, 42), onNext(25, 99)), scheduler.createColdObservable(onNext(60, 42), onNext(65, 99)), scheduler.createColdObservable(onNext(20, 42), onNext(25, 99))];
-      var results = scheduler.startScheduler(function () {
-          return xs.debounce(function (x) {
-              return ys[x];
-          });
+  test('debounce duration debounce behavior', function () {
+    var scheduler = new TestScheduler();
+
+    var xs = scheduler.createHotObservable(
+      onNext(150, -1),
+      onNext(250, 0),
+      onNext(280, 1),
+      onNext(310, 2),
+      onNext(350, 3),
+      onNext(400, 4),
+      onCompleted(550));
+
+    var ys = [
+      scheduler.createColdObservable(onNext(20, 42), onNext(25, 99)),
+      scheduler.createColdObservable(onNext(40, 42), onNext(45, 99)),
+      scheduler.createColdObservable(onNext(20, 42), onNext(25, 99)),
+      scheduler.createColdObservable(onNext(60, 42), onNext(65, 99)),
+      scheduler.createColdObservable(onNext(20, 42), onNext(25, 99))];
+
+    var results = scheduler.startScheduler(function () {
+      return xs.debounce(function (x) {
+        return ys[x];
       });
-      results.messages.assertEqual(onNext(250 + 20, 0), onNext(310 + 20, 2), onNext(400 + 20, 4), onCompleted(550));
-      xs.subscriptions.assertEqual(subscribe(200, 550));
-      ys[0].subscriptions.assertEqual(subscribe(250, 250 + 20));
-      ys[1].subscriptions.assertEqual(subscribe(280, 310));
-      ys[2].subscriptions.assertEqual(subscribe(310, 310 + 20));
-      ys[3].subscriptions.assertEqual(subscribe(350, 400));
-      ys[4].subscriptions.assertEqual(subscribe(400, 400 + 20));
+    });
+
+    results.messages.assertEqual(
+      onNext(250 + 20, 0),
+      onNext(310 + 20, 2),
+      onNext(400 + 20, 4),
+      onCompleted(550));
+
+    xs.subscriptions.assertEqual(subscribe(200, 550));
+
+    ys[0].subscriptions.assertEqual(subscribe(250, 250 + 20));
+    ys[1].subscriptions.assertEqual(subscribe(280, 310));
+    ys[2].subscriptions.assertEqual(subscribe(310, 310 + 20));
+    ys[3].subscriptions.assertEqual(subscribe(350, 400));
+    ys[4].subscriptions.assertEqual(subscribe(400, 400 + 20));
   });
 
-  test('debounce Duration EarlyCompletion', function () {
-      var scheduler = new TestScheduler();
-      var xs = scheduler.createHotObservable(onNext(150, -1), onNext(250, 0), onNext(280, 1), onNext(310, 2), onNext(350, 3), onNext(400, 4), onCompleted(410));
-      var ys = [scheduler.createColdObservable(onNext(20, 42), onNext(25, 99)), scheduler.createColdObservable(onNext(40, 42), onNext(45, 99)), scheduler.createColdObservable(onNext(20, 42), onNext(25, 99)), scheduler.createColdObservable(onNext(60, 42), onNext(65, 99)), scheduler.createColdObservable(onNext(20, 42), onNext(25, 99))];
-      var results = scheduler.startScheduler(function () {
-          return xs.debounce(function (x) {
-              return ys[x];
-          });
-      });
-      results.messages.assertEqual(onNext(250 + 20, 0), onNext(310 + 20, 2), onNext(410, 4), onCompleted(410));
-      xs.subscriptions.assertEqual(subscribe(200, 410));
-      ys[0].subscriptions.assertEqual(subscribe(250, 250 + 20));
-      ys[1].subscriptions.assertEqual(subscribe(280, 310));
-      ys[2].subscriptions.assertEqual(subscribe(310, 310 + 20));
-      ys[3].subscriptions.assertEqual(subscribe(350, 400));
-      ys[4].subscriptions.assertEqual(subscribe(400, 410));
+  test('debounce duration early completion', function () {
+    var scheduler = new TestScheduler();
+
+    var xs = scheduler.createHotObservable(
+      onNext(150, -1),
+      onNext(250, 0),
+      onNext(280, 1),
+      onNext(310, 2),
+      onNext(350, 3),
+      onNext(400, 4),
+      onCompleted(410));
+
+    var ys = [
+      scheduler.createColdObservable(onNext(20, 42), onNext(25, 99)),
+      scheduler.createColdObservable(onNext(40, 42), onNext(45, 99)),
+      scheduler.createColdObservable(onNext(20, 42), onNext(25, 99)),
+      scheduler.createColdObservable(onNext(60, 42), onNext(65, 99)),
+      scheduler.createColdObservable(onNext(20, 42), onNext(25, 99))];
+
+    var results = scheduler.startScheduler(function () {
+      return xs.debounce(function (x) { return ys[x]; });
+    });
+
+    results.messages.assertEqual(
+      onNext(250 + 20, 0),
+      onNext(310 + 20, 2),
+      onNext(410, 4),
+      onCompleted(410));
+
+    xs.subscriptions.assertEqual(subscribe(200, 410));
+    ys[0].subscriptions.assertEqual(subscribe(250, 250 + 20));
+    ys[1].subscriptions.assertEqual(subscribe(280, 310));
+    ys[2].subscriptions.assertEqual(subscribe(310, 310 + 20));
+    ys[3].subscriptions.assertEqual(subscribe(350, 400));
+    ys[4].subscriptions.assertEqual(subscribe(400, 410));
   });
 
-  test('debounce Duration InnerError', function () {
-      var scheduler = new TestScheduler();
-      var xs = scheduler.createHotObservable(onNext(150, 1), onNext(250, 2), onNext(350, 3), onNext(450, 4), onCompleted(550));
+  test('debounce duration inner error', function () {
+    var scheduler = new TestScheduler();
+
+    var xs = scheduler.createHotObservable(
+      onNext(150, 1),
+      onNext(250, 2),
+      onNext(350, 3),
+      onNext(450, 4),
+      onCompleted(550));
+
+    var error = new Error();
+
+    var results = scheduler.startScheduler(function () {
+      return xs.debounce(function (x) {
+        if (x < 4) {
+          return scheduler.createColdObservable(onNext(x * 10, 'Ignore'), onNext(x * 10 + 5, 'Aargh!'));
+        } else {
+          return scheduler.createColdObservable(onError(x * 10, error));
+        }
+      });
+    });
+
+    results.messages.assertEqual(
+      onNext(250 + 2 * 10, 2),
+      onNext(350 + 3 * 10, 3),
+       onError(450 + 4 * 10, error));
+
+    xs.subscriptions.assertEqual(
+      subscribe(200, 490));
+  });
+
+  test('debounce duration outer error', function () {
       var error = new Error();
-      var results = scheduler.startScheduler(function () {
-          return xs.debounce(function (x) {
-              if (x < 4) {
-                  return scheduler.createColdObservable(onNext(x * 10, 'Ignore'), onNext(x * 10 + 5, 'Aargh!'));
-              } else {
-                  return scheduler.createColdObservable(onError(x * 10, error));
-              }
-          });
-      });
-      results.messages.assertEqual(onNext(250 + 2 * 10, 2), onNext(350 + 3 * 10, 3), onError(450 + 4 * 10, error));
-      xs.subscriptions.assertEqual(subscribe(200, 490));
-  });
 
-  test('debounce Duration OuterError', function () {
-      var error = new Error();
       var scheduler = new TestScheduler();
-      var xs = scheduler.createHotObservable(onNext(150, 1), onNext(250, 2), onNext(350, 3), onNext(450, 4), onError(460, error));
+
+      var xs = scheduler.createHotObservable(
+        onNext(150, 1),
+        onNext(250, 2),
+        onNext(350, 3),
+        onNext(450, 4),
+        onError(460, error));
+
       var results = scheduler.startScheduler(function () {
-          return xs.debounce(function (x) {
-              return scheduler.createColdObservable(onNext(x * 10, 'Ignore'), onNext(x * 10 + 5, 'Aargh!'));
-          });
+        return xs.debounce(function (x) {
+          return scheduler.createColdObservable(onNext(x * 10, 'Ignore'), onNext(x * 10 + 5, 'Aargh!'));
+        });
       });
-      results.messages.assertEqual(onNext(250 + 2 * 10, 2), onNext(350 + 3 * 10, 3), onError(460, error));
-      xs.subscriptions.assertEqual(subscribe(200, 460));
+
+      results.messages.assertEqual(
+        onNext(250 + 2 * 10, 2),
+        onNext(350 + 3 * 10, 3),
+        onError(460, error));
+
+      xs.subscriptions.assertEqual(
+        subscribe(200, 460));
   });
 
   test('debounce duration selector throws', function () {
@@ -339,6 +409,7 @@
         throw error;
       });
     });
+
     results.messages.assertEqual(
       onNext(250 + 2 * 10, 2),
       onNext(350 + 3 * 10, 3),
--- tests/observable/delay.js
+++ tests/observable/delay.js
@@ -4,8 +4,7 @@
 (function () {
   QUnit.module('delay');
 
-  var Observable = Rx.Observable,
-      TestScheduler = Rx.TestScheduler,
+  var TestScheduler = Rx.TestScheduler,
       onNext = Rx.ReactiveTest.onNext,
       onError = Rx.ReactiveTest.onError,
       onCompleted = Rx.ReactiveTest.onCompleted,
@@ -307,4 +306,204 @@
       subscribe(200, 1000));
   });
 
+  // Delay with selector
+  test('delay duration simple 1', function () {
+    var scheduler = new TestScheduler();
+
+    var xs = scheduler.createHotObservable(
+      onNext(150, 1),
+      onNext(210, 10),
+      onNext(220, 30),
+      onNext(230, 50),
+      onNext(240, 35),
+      onNext(250, 20),
+      onCompleted(260));
+
+    var results = scheduler.startWithCreate(function () {
+      return xs.delayWithSelector(function (x) {
+        return scheduler.createColdObservable(onNext(x, '!'));
+      });
+    });
+
+    results.messages.assertEqual(
+      onNext(210 + 10, 10),
+      onNext(220 + 30, 30),
+      onNext(250 + 20, 20),
+      onNext(240 + 35, 35),
+      onNext(230 + 50, 50),
+      onCompleted(280));
+
+    xs.subscriptions.assertEqual(subscribe(200, 260));
+  });
+
+  test('delay duration simple 2', function () {
+    var scheduler = new TestScheduler();
+
+    var xs = scheduler.createHotObservable(
+      onNext(150, 1),
+      onNext(210, 2),
+      onNext(220, 3),
+      onNext(230, 4),
+      onNext(240, 5),
+      onNext(250, 6),
+      onCompleted(300));
+
+    var ys = scheduler.createColdObservable(onNext(10, '!'));
+
+    var results = scheduler.startWithCreate(function () {
+      return xs.delayWithSelector(function () {
+        return ys;
+      });
+    });
+
+    results.messages.assertEqual(
+      onNext(210 + 10, 2),
+      onNext(220 + 10, 3),
+      onNext(230 + 10, 4),
+      onNext(240 + 10, 5),
+      onNext(250 + 10, 6),
+      onCompleted(300));
+
+    xs.subscriptions.assertEqual(subscribe(200, 300));
+
+    ys.subscriptions.assertEqual(
+      subscribe(210, 220),
+      subscribe(220, 230),
+      subscribe(230, 240),
+      subscribe(240, 250),
+      subscribe(250, 260));
+  });
+
+  test('delay duration simple 3', function () {
+    var scheduler = new TestScheduler();
+
+    var xs = scheduler.createHotObservable(
+      onNext(150, 1),
+      onNext(210, 2),
+      onNext(220, 3),
+      onNext(230, 4),
+      onNext(240, 5),
+      onNext(250, 6),
+      onCompleted(300));
+
+    var ys = scheduler.createColdObservable(onNext(100, '!'));
+
+    var results = scheduler.startWithCreate(function () {
+      return xs.delayWithSelector(function () {
+        return ys;
+      });
+    });
+
+    results.messages.assertEqual(
+      onNext(210 + 100, 2),
+      onNext(220 + 100, 3),
+      onNext(230 + 100, 4),
+      onNext(240 + 100, 5),
+      onNext(250 + 100, 6),
+      onCompleted(350));
+
+    xs.subscriptions.assertEqual(subscribe(200, 300));
+
+    ys.subscriptions.assertEqual(
+      subscribe(210, 310),
+      subscribe(220, 320),
+      subscribe(230, 330),
+      subscribe(240, 340),
+      subscribe(250, 350));
+  });
+
+  test('delay duration simple 4 inner empty', function () {
+    var scheduler = new TestScheduler();
+
+    var xs = scheduler.createHotObservable(
+      onNext(150, 1),
+      onNext(210, 2),
+      onNext(220, 3),
+      onNext(230, 4),
+      onNext(240, 5),
+      onNext(250, 6),
+      onCompleted(300));
+
+    var ys = scheduler.createColdObservable(onCompleted(100));
+    var results = scheduler.startWithCreate(function () {
+      return xs.delayWithSelector(function () {
+        return ys;
+      });
+    });
+
+    results.messages.assertEqual(
+      onNext(210 + 100, 2),
+      onNext(220 + 100, 3),
+      onNext(230 + 100, 4),
+      onNext(240 + 100, 5),
+      onNext(250 + 100, 6),
+      onCompleted(350));
+
+    xs.subscriptions.assertEqual(subscribe(200, 300));
+
+    ys.subscriptions.assertEqual(
+      subscribe(210, 310),
+      subscribe(220, 320),
+      subscribe(230, 330),
+      subscribe(240, 340),
+      subscribe(250, 350));
+  });
+
+  test('delay duration dispose 1', function () {
+    var scheduler = new TestScheduler();
+
+    var xs = scheduler.createHotObservable(
+      onNext(150, 1),
+      onNext(210, 2),
+      onNext(220, 3),
+      onNext(230, 4),
+      onNext(240, 5),
+      onNext(250, 6),
+      onCompleted(300));
+
+    var ys = scheduler.createColdObservable(onNext(200, '!'));
+
+    var results = scheduler.startScheduler(function () {
+        return xs.delayWithSelector(function () {
+          return ys;
+        });
+    }, { disposed: 425 });
+
+    results.messages.assertEqual(
+      onNext(210 + 200, 2),
+      onNext(220 + 200, 3));
+
+    xs.subscriptions.assertEqual(subscribe(200, 300));
+
+    ys.subscriptions.assertEqual(
+      subscribe(210, 410),
+      subscribe(220, 420),
+      subscribe(230, 425),
+      subscribe(240, 425),
+      subscribe(250, 425));
+  });
+
+  test('delay duration dispose 2', function () {
+    var scheduler = new TestScheduler();
+
+    var xs = scheduler.createHotObservable(
+      onNext(150, 1),
+      onNext(210, 2),
+      onNext(400, 3),
+      onCompleted(500));
+
+    var ys = scheduler.createColdObservable(onNext(50, '!'));
+
+    var results = scheduler.startScheduler(function () {
+      return xs.delayWithSelector(function () {
+        return ys;
+      });
+    }, {disposed: 300 });
+
+    results.messages.assertEqual(onNext(210 + 50, 2));
+
+    xs.subscriptions.assertEqual(subscribe(200, 300));
+
+    ys.subscriptions.assertEqual(subscribe(210, 260));
+  });
 }());
