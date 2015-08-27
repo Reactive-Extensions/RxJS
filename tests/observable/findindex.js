87b396d89097585ad6175ca88fb846db3187b89a
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2015-08-27 01:54:15 -0400

    Fixing findIndex

--- tests/observable/findindex.js
+++ tests/observable/findindex.js
@@ -1,122 +1,123 @@
-QUnit.module('FindIndex');
+(function () {
+  /* jshint undef: true, unused: true */
+  /* globals QUnit, test, Rx */
 
-var TestScheduler = Rx.TestScheduler,
-    onNext = Rx.ReactiveTest.onNext,
-    onError = Rx.ReactiveTest.onError,
-    onCompleted = Rx.ReactiveTest.onCompleted,
-    subscribe = Rx.ReactiveTest.subscribe;
+  QUnit.module('findIndex');
 
-test('findIndex_Never', function () {
+  var TestScheduler = Rx.TestScheduler,
+      onNext = Rx.ReactiveTest.onNext,
+      onError = Rx.ReactiveTest.onError,
+      onCompleted = Rx.ReactiveTest.onCompleted;
+
+  test('findIndex never', function () {
     var scheduler = new TestScheduler();
     var xs = scheduler.createHotObservable(
-        onNext(150, 1)
+      onNext(150, 1)
     );
 
-    var res = scheduler.startWithCreate(function () {
-        return xs.findIndex(function () {
-            return true;
-        });
+    var res = scheduler.startScheduler(function () {
+      return xs.findIndex(function () { return true; });
     });
 
     res.messages.assertEqual(
     );
-});
+  });
 
-test('findIndex_Empty', function () {
+  test('findIndex empty', function () {
     var scheduler = new TestScheduler();
+
     var xs = scheduler.createHotObservable(
-        onNext(150, 1),
-        onCompleted(210)
+      onNext(150, 1),
+      onCompleted(210)
     );
 
-    var res = scheduler.startWithCreate(function () {
-        return xs.findIndex(function () {
-            return true;
-        });
+    var res = scheduler.startScheduler(function () {
+      return xs.findIndex(function () { return true; });
     });
 
     res.messages.assertEqual(
-        onNext(210, -1),
-        onCompleted(210)
+      onNext(210, -1),
+      onCompleted(210)
     );
-});
+  });
 
-test('findIndex_Single', function () {
+  test('findIndex single', function () {
     var scheduler = new TestScheduler();
+
     var xs = scheduler.createHotObservable(
-        onNext(150, 1),
-        onNext(210, 2),
-        onCompleted(220)
+      onNext(150, 1),
+      onNext(210, 2),
+      onCompleted(220)
     );
 
-    var res = scheduler.startWithCreate(function () {
-        return xs.findIndex(function (x) {
-            return x === 2;
-        });
+    var res = scheduler.startScheduler(function () {
+      return xs.findIndex(function (x) { return x === 2; });
     });
 
     res.messages.assertEqual(
-        onNext(210, 0),
-        onCompleted(210)
+      onNext(210, 0),
+      onCompleted(210)
     );
-});
+  });
 
-test('findIndex_NotFound', function () {
+  test('findIndex_NotFound', function () {
+      var scheduler = new TestScheduler();
+      var xs = scheduler.createHotObservable(
+          onNext(150, 1),
+          onNext(210, 2),
+          onCompleted(220)
+      );
+
+      var res = scheduler.startScheduler(function () {
+          return xs.findIndex(function (x) {
+              return x === 3;
+          });
+      });
+
+      res.messages.assertEqual(
+          onNext(220, -1),
+          onCompleted(220)
+      );
+  });
+
+  test('findIndex_Error', function () {
+      var error = new Error();
+      var scheduler = new TestScheduler();
+      var xs = scheduler.createHotObservable(
+          onNext(150, 1),
+          onNext(210, 2),
+          onError(220, error)
+      );
+
+      var res = scheduler.startScheduler(function () {
+          return xs.findIndex(function (x) {
+              return x === 3;
+          });
+      });
+
+      res.messages.assertEqual(
+          onError(220, error)
+      );
+  });
+
+  test('findIndex_Throws', function () {
+    var error = new Error();
+
     var scheduler = new TestScheduler();
+
     var xs = scheduler.createHotObservable(
-        onNext(150, 1),
-        onNext(210, 2),
-        onCompleted(220)
+      onNext(150, 1),
+      onNext(210, 2),
+      onCompleted(220)
     );
 
-    var res = scheduler.startWithCreate(function () {
-        return xs.findIndex(function (x) {
-            return x === 3;
-        });
+    var res = scheduler.startScheduler(function () {
+      return xs.findIndex(function () { throw error; });
     });
 
     res.messages.assertEqual(
-        onNext(220, -1),
-        onCompleted(220)
+      onError(210, error)
     );
-});
+  });
 
-test('findIndex_Error', function () {
-    var ex = new Error('error');
-    var scheduler = new TestScheduler();
-    var xs = scheduler.createHotObservable(
-        onNext(150, 1),
-        onNext(210, 2),
-        onError(220, ex)
-    );
-
-    var res = scheduler.startWithCreate(function () {
-        return xs.findIndex(function (x) {
-            return x === 3;
-        });
-    });
-
-    res.messages.assertEqual(
-        onError(220, ex)
-    );
-});
-
-test('findIndex_Throws', function () {
-    var ex = new Error('error');
-    var scheduler = new TestScheduler();
-    var xs = scheduler.createHotObservable(
-        onNext(150, 1),
-        onNext(210, 2),
-        onCompleted(220)
-    );
-
-    var res = scheduler.startWithCreate(function () {
-        return xs.findIndex(function (x) {
-            throw ex;
-        });
-    });
-
-    res.messages.assertEqual(
-        onError(210, ex)
-    );
-});
+}());
