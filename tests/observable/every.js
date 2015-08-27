0f457955a3b45a4c68fff8130de56585c6ea7742
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2015-08-27 01:23:13 -0400

    Fixing every

--- tests/observable/every.js
+++ tests/observable/every.js
@@ -1,163 +1,162 @@
 (function () {
-  (function () {
-    QUnit.module('every');
+  /* jshint undef: true, unused: true */
+  /* globals QUnit, test, Rx */
 
-    var TestScheduler = Rx.TestScheduler,
-      onNext = Rx.ReactiveTest.onNext,
-      onError = Rx.ReactiveTest.onError,
-      onCompleted = Rx.ReactiveTest.onCompleted,
-      subscribe = Rx.ReactiveTest.subscribe;
+  QUnit.module('every');
 
-    test('every_Empty', function () {
-      var scheduler = new TestScheduler();
+  var TestScheduler = Rx.TestScheduler,
+    onNext = Rx.ReactiveTest.onNext,
+    onError = Rx.ReactiveTest.onError,
+    onCompleted = Rx.ReactiveTest.onCompleted;
 
-      var xs = scheduler.createHotObservable(
-        onNext(150, 1),
-        onCompleted(250)
-      );
+  test('every empty', function () {
+    var scheduler = new TestScheduler();
 
-      var results = scheduler.startWithCreate(function () {
+    var xs = scheduler.createHotObservable(
+      onNext(150, 1),
+      onCompleted(250)
+    );
+
+    var results = scheduler.startScheduler(function () {
+      return xs.every(function (x) { return x > 0; });
+    });
+
+    results.messages.assertEqual(
+      onNext(250, true),
+      onCompleted(250)
+    );
+  });
+
+  test('every return', function () {
+    var scheduler = new TestScheduler();
+
+    var xs = scheduler.createHotObservable(
+      onNext(150, 1),
+      onNext(210, 2),
+      onCompleted(250)
+    );
+
+    var results = scheduler.startScheduler(function () {
+      return xs.every(function (x) { return x > 0; });
+    });
+
+    results.messages.assertEqual(
+      onNext(250, true),
+      onCompleted(250)
+    );
+  });
+
+  test('every return no match', function () {
+    var scheduler = new TestScheduler();
+
+    var xs = scheduler.createHotObservable(
+      onNext(150, 1),
+      onNext(210, -2),
+      onCompleted(250)
+    );
+
+    var results = scheduler.startScheduler(function () {
+      return xs.every(function (x) { return x > 0; });
+    });
+
+    results.messages.assertEqual(
+      onNext(210, false),
+      onCompleted(210)
+    );
+  });
+
+  test('every none match', function () {
+    var scheduler = new TestScheduler();
+
+    var xs = scheduler.createHotObservable(
+      onNext(150, 1),
+      onNext(210, -2),
+      onNext(220, -3),
+      onNext(230, -4),
+      onCompleted(250)
+    );
+
+    var results = scheduler.startScheduler(function () {
+      return xs.every(function (x) { return x > 0; });
+    });
+
+    results.messages.assertEqual(
+      onNext(210, false),
+      onCompleted(210)
+    );
+  });
+
+  test('every some match', function () {
+    var scheduler = new TestScheduler();
+
+    var xs = scheduler.createHotObservable(
+      onNext(150, 1),
+      onNext(210, -2),
+      onNext(220, 3),
+      onNext(230, -4),
+      onCompleted(250)
+    );
+
+    var results = scheduler.startScheduler(function () {
         return xs.every(function (x) { return x > 0; });
-      });
-
-      results.messages.assertEqual(
-        onNext(250, true),
-        onCompleted(250)
-      );
     });
 
-    test('every_Return', function () {
-      var scheduler = new TestScheduler();
+    results.messages.assertEqual(
+      onNext(210, false),
+      onCompleted(210)
+    );
+  });
 
-      var xs = scheduler.createHotObservable(
-        onNext(150, 1),
-        onNext(210, 2),
-        onCompleted(250)
-      );
+  test('every all match', function () {
+    var scheduler = new TestScheduler();
 
-      var results = scheduler.startWithCreate(function () {
-        return xs.every(function (x) { return x > 0; });
-      });
+    var xs = scheduler.createHotObservable(
+      onNext(150, 1),
+      onNext(210, 2),
+      onNext(220, 3),
+      onNext(230, 4),
+      onCompleted(250)
+    );
 
-      results.messages.assertEqual(
-        onNext(250, true),
-        onCompleted(250)
-      );
+    var results = scheduler.startScheduler(function () {
+      return xs.every(function (x) { return x > 0; });
     });
 
-    test('every_ReturnNotMatch', function () {
-      var scheduler = new TestScheduler();
+    results.messages.assertEqual(
+      onNext(250, true),
+      onCompleted(250)
+    );
+  });
 
-      var xs = scheduler.createHotObservable(
-        onNext(150, 1),
-        onNext(210, -2),
-        onCompleted(250)
-      );
+  test('every throw', function () {
+    var error = new Error();
+    var scheduler = new TestScheduler();
 
-      var results = scheduler.startWithCreate(function () {
-        return xs.every(function (x) { return x > 0; });
-      });
+    var xs = scheduler.createHotObservable(
+      onNext(150, 1),
+      onError(210, error)
+    );
 
-      results.messages.assertEqual(
-        onNext(210, false),
-        onCompleted(210)
-      );
+    var results = scheduler.startScheduler(function () {
+      return xs.every(function (x) { return x > 0; });
     });
 
-    test('every_SomeNoneMatch', function () {
-      var scheduler = new TestScheduler();
+    results.messages.assertEqual(
+      onError(210, error)
+    );
+  });
 
-      var xs = scheduler.createHotObservable(
-        onNext(150, 1),
-        onNext(210, -2),
-        onNext(220, -3),
-        onNext(230, -4),
-        onCompleted(250)
-      );
+  test('every never', function () {
+    var scheduler = new TestScheduler();
 
-      var results = scheduler.startWithCreate(function () {
-        return xs.every(function (x) { return x > 0; });
-      });
+    var xs = scheduler.createHotObservable(
+      onNext(150, 1)
+    );
 
-      results.messages.assertEqual(
-        onNext(210, false),
-        onCompleted(210)
-      );
+    var results = scheduler.startScheduler(function () {
+      return xs.every(function (x) { return x > 0; });
     });
 
-    test('every_SomeMatch', function () {
-      var scheduler = new TestScheduler();
-
-      var xs = scheduler.createHotObservable(
-        onNext(150, 1),
-        onNext(210, -2),
-        onNext(220, 3),
-        onNext(230, -4),
-        onCompleted(250)
-      );
-
-      var results = scheduler.startWithCreate(function () {
-          return xs.every(function (x) { return x > 0; });
-      });
-
-      results.messages.assertEqual(
-        onNext(210, false),
-        onCompleted(210)
-      );
-    });
-
-    test('every_SomeeveryMatch', function () {
-      var scheduler = new TestScheduler();
-
-      var xs = scheduler.createHotObservable(
-        onNext(150, 1),
-        onNext(210, 2),
-        onNext(220, 3),
-        onNext(230, 4),
-        onCompleted(250)
-      );
-
-      var results = scheduler.startWithCreate(function () {
-        return xs.every(function (x) { return x > 0; });
-      });
-
-      results.messages.assertEqual(
-        onNext(250, true),
-        onCompleted(250)
-      );
-    });
-
-    test('every_Throw', function () {
-      var error = new Error();
-      var scheduler = new TestScheduler();
-
-      var xs = scheduler.createHotObservable(
-        onNext(150, 1),
-        onError(210, error)
-      );
-
-      var results = scheduler.startWithCreate(function () {
-        return xs.every(function (x) { return x > 0; });
-      });
-
-      results.messages.assertEqual(
-        onError(210, error)
-      );
-    });
-
-    test('every_Never', function () {
-      var scheduler = new TestScheduler();
-
-      var xs = scheduler.createHotObservable(
-        onNext(150, 1)
-      );
-
-      var results = scheduler.startWithCreate(function () {
-        return xs.every(function (x) { return x > 0; });
-      });
-
-      results.messages.assertEqual();
-    });
-  }());
-  
+    results.messages.assertEqual();
+  });
 }());
