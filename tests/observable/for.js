34b0cf3cdf684a1aa9dd8e4cbb51fe9695f97681
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2015-08-27 02:02:10 -0400

    Fixing for

--- tests/observable/for.js
+++ tests/observable/for.js
@@ -1,51 +1,56 @@
-QUnit.module('For');
+(function () {
+  /* jshint undef: true, unused: true */
+  /* globals QUnit, test, Rx */
 
-var Observable = Rx.Observable,
-  TestScheduler = Rx.TestScheduler,
-  onNext = Rx.ReactiveTest.onNext,
-  onError = Rx.ReactiveTest.onError,
-  onCompleted = Rx.ReactiveTest.onCompleted,
-  subscribe = Rx.ReactiveTest.subscribe;
+  QUnit.module('for');
 
-test('For_Basic', function () {
-  var scheduler = new TestScheduler();
+  var Observable = Rx.Observable,
+    TestScheduler = Rx.TestScheduler,
+    onNext = Rx.ReactiveTest.onNext,
+    onError = Rx.ReactiveTest.onError,
+    onCompleted = Rx.ReactiveTest.onCompleted;
 
-  var results = scheduler.startWithCreate(function () {
-    return Observable.forIn([1, 2, 3], function (x) {
-      return scheduler.createColdObservable(
-        onNext(x * 100 + 10, x * 10 + 1),
-        onNext(x * 100 + 20, x * 10 + 2),
-        onNext(x * 100 + 30, x * 10 + 3),
-        onCompleted(x * 100 + 40)
-      );
+  test('for basic', function () {
+    var scheduler = new TestScheduler();
+
+    var results = scheduler.startScheduler(function () {
+      return Observable['for']([1, 2, 3], function (x) {
+        return scheduler.createColdObservable(
+          onNext(x * 100 + 10, x * 10 + 1),
+          onNext(x * 100 + 20, x * 10 + 2),
+          onNext(x * 100 + 30, x * 10 + 3),
+          onCompleted(x * 100 + 40)
+        );
+      });
     });
+
+    results.messages.assertEqual(
+      onNext(310, 11),
+      onNext(320, 12),
+      onNext(330, 13),
+      onNext(550, 21),
+      onNext(560, 22),
+      onNext(570, 23),
+      onNext(890, 31),
+      onNext(900, 32),
+      onNext(910, 33),
+      onCompleted(920)
+    );
   });
 
-  results.messages.assertEqual(
-    onNext(310, 11),
-    onNext(320, 12),
-    onNext(330, 13),
-    onNext(550, 21),
-    onNext(560, 22),
-    onNext(570, 23),
-    onNext(890, 31),
-    onNext(900, 32),
-    onNext(910, 33),
-    onCompleted(920)
-  );
-});
+  test('for throws', function () {
+    var error = new Error();
 
-test('For_Throws', function () {
-  var error = new Error();
+    var scheduler = new TestScheduler();
 
-  var scheduler = new TestScheduler();
-
-  var results = scheduler.startWithCreate(function () {
-    return Observable.forIn([1, 2, 3], function () {
-      throw error;
+    var results = scheduler.startScheduler(function () {
+      return Observable['for']([1, 2, 3], function () {
+        throw error;
+      });
     });
+    results.messages.assertEqual(
+      onError(200, error)
+    );
   });
-  results.messages.assertEqual(
-    onError(200, error)
-  );
-});
+
+}());
