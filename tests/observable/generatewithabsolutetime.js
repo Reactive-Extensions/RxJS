d5b6623d1013a8d9db991c10a1454af963fd60b1
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2015-08-28 15:18:44 -0400

    Fixing generateWithTIme

--- tests/observable/generatewithabsolutetime.js
+++ tests/observable/generatewithabsolutetime.js
@@ -1,114 +1,142 @@
-QUnit.module('GenerateWithAbsoluteTime');
+(function () {
+  /* jshint undef: true, unused: true */
+  /* globals QUnit, test, Rx */
 
-var Observable = Rx.Observable,
-    TestScheduler = Rx.TestScheduler,
-    onNext = Rx.ReactiveTest.onNext,
-    onError = Rx.ReactiveTest.onError,
-    onCompleted = Rx.ReactiveTest.onCompleted,
-    subscribe = Rx.ReactiveTest.subscribe;
+  QUnit.module('generateWithAbsoluteTime');
 
-test('Generate_DateTimeOffset_Finite', function () {
-    var results, scheduler;
-    scheduler = new TestScheduler();
-    results = scheduler.startWithCreate(function () {
-        return Rx.Observable.generateWithAbsoluteTime(0, function (x) {
+  var Observable = Rx.Observable,
+      TestScheduler = Rx.TestScheduler,
+      onNext = Rx.ReactiveTest.onNext,
+      onError = Rx.ReactiveTest.onError,
+      onCompleted = Rx.ReactiveTest.onCompleted;
+
+  test('generateWithAbsoluteTime absolute time finite', function () {
+    var scheduler = new TestScheduler();
+
+    var results = scheduler.startScheduler(function () {
+        return Observable.generateWithAbsoluteTime(0, function (x) {
             return x <= 3;
         }, function (x) {
             return x + 1;
         }, function (x) {
             return x;
         }, function (x) {
-            return scheduler.now() + x + 1;
+            return new Date(scheduler.now() + x + 1);
         }, scheduler);
     });
-    results.messages.assertEqual(onNext(202, 0), onNext(204, 1), onNext(207, 2), onNext(211, 3), onCompleted(211));
-});
 
-test('Generate_DateTimeOffset_Throw_Condition', function () {
-    var ex, results, scheduler;
-    ex = 'ex';
-    scheduler = new TestScheduler();
-    results = scheduler.startWithCreate(function () {
-        return Rx.Observable.generateWithAbsoluteTime(0, function (x) {
-            throw ex;
-        }, function (x) {
-            return x + 1;
+    results.messages.assertEqual(
+      onNext(202, 0),
+      onNext(204, 1),
+      onNext(207, 2),
+      onNext(211, 3),
+      onCompleted(211));
+  });
+
+  test('generateWithAbsoluteTime absolute time throw condition', function () {
+    var error = new Error();
+
+    var scheduler = new TestScheduler();
+
+    var results = scheduler.startScheduler(function () {
+      return Observable.generateWithAbsoluteTime(0, function () {
+        throw error;
+      }, function (x) {
+        return x + 1;
+      }, function (x) {
+        return x;
+      }, function (x) {
+        return new Date(scheduler.now() + x + 1);
+      }, scheduler);
+    });
+
+    results.messages.assertEqual(
+      onError(201, error));
+  });
+
+  test('generateWithAbsoluteTime absolute time throw result selector', function () {
+    var error = new Error();
+
+    var scheduler = new TestScheduler();
+
+    var results = scheduler.startScheduler(function () {
+      return Observable.generateWithAbsoluteTime(0, function () {
+        return true;
+      }, function (x) {
+        return x + 1;
+      }, function () {
+        throw error;
+      }, function (x) {
+        return new Date(scheduler.now() + x + 1);
+      }, scheduler);
+    });
+
+    results.messages.assertEqual(
+      onError(201, error));
+  });
+
+  test('generateWithAbsoluteTime absolute time throw iterate', function () {
+    var error = new Error();
+
+    var scheduler = new TestScheduler();
+
+    var results = scheduler.startScheduler(function () {
+        return Observable.generateWithAbsoluteTime(0, function () {
+            return true;
+        }, function () {
+            throw error;
         }, function (x) {
             return x;
         }, function (x) {
-            return scheduler.now() + x + 1;
+            return new Date(scheduler.now() + x + 1);
         }, scheduler);
     });
-    results.messages.assertEqual(onError(201, ex));
-});
 
-test('Generate_DateTimeOffset_Throw_ResultSelector', function () {
-    var ex, results, scheduler;
-    ex = 'ex';
-    scheduler = new TestScheduler();
-    results = scheduler.startWithCreate(function () {
-        return Rx.Observable.generateWithAbsoluteTime(0, function (x) {
-            return true;
+    results.messages.assertEqual(
+      onNext(202, 0),
+      onError(202, error));
+  });
+
+  test('generateWithAbsoluteTime absolute time throw time selector', function () {
+    var error = new Error();
+
+    var scheduler = new TestScheduler();
+
+    var results = scheduler.startScheduler(function () {
+        return Observable.generateWithAbsoluteTime(0, function () {
+          return true;
         }, function (x) {
-            return x + 1;
+          return x + 1;
         }, function (x) {
-            throw ex;
-        }, function (x) {
-            return scheduler.now() + x + 1;
+          return x;
+        }, function () {
+          throw error;
         }, scheduler);
     });
-    results.messages.assertEqual(onError(201, ex));
-});
 
-test('Generate_DateTimeOffset_Throw_Iterate', function () {
-    var ex, results, scheduler;
-    ex = 'ex';
-    scheduler = new TestScheduler();
-    results = scheduler.startWithCreate(function () {
-        return Rx.Observable.generateWithAbsoluteTime(0, function (x) {
-            return true;
-        }, function (x) {
-            throw ex;
-        }, function (x) {
-            return x;
-        }, function (x) {
-            return scheduler.now() + x + 1;
-        }, scheduler);
-    });
-    results.messages.assertEqual(onNext(202, 0), onError(202, ex));
-});
+    results.messages.assertEqual(
+      onError(201, error));
+  });
 
-test('Generate_DateTimeOffset_Throw_TimeSelector', function () {
-    var ex, results, scheduler;
-    ex = 'ex';
-    scheduler = new TestScheduler();
-    results = scheduler.startWithCreate(function () {
-        return Rx.Observable.generateWithAbsoluteTime(0, function (x) {
-            return true;
-        }, function (x) {
-            return x + 1;
-        }, function (x) {
-            return x;
-        }, function (x) {
-            throw ex;
-        }, scheduler);
-    });
-    results.messages.assertEqual(onError(201, ex));
-});
+  test('generateWithAbsoluteTime absolute time dispose', function () {
+    var scheduler = new TestScheduler();
 
-test('Generate_DateTimeOffset_Dispose', function () {
-    var results, scheduler;
-    scheduler = new TestScheduler();
-    results = scheduler.startWithDispose(function () {
-        return Rx.Observable.generateWithAbsoluteTime(0, function (x) {
-            return true;
+    var results = scheduler.startScheduler(function () {
+        return Observable.generateWithAbsoluteTime(0, function () {
+          return true;
         }, function (x) {
-            return x + 1;
+          return x + 1;
         }, function (x) {
-            return x;
+          return x;
         }, function (x) {
-            return scheduler.now() + x + 1;
+          return new Date(scheduler.now() + x + 1);
         }, scheduler);
-    }, 210);
-    results.messages.assertEqual(onNext(202, 0), onNext(204, 1), onNext(207, 2));
-});
+    }, { disposed: 210 });
+
+    results.messages.assertEqual(
+      onNext(202, 0),
+      onNext(204, 1),
+      onNext(207, 2));
+  });
+
+}());
--- tests/observable/generatewithrelativetime.js
+++ tests/observable/generatewithrelativetime.js
@@ -1,114 +1,142 @@
-QUnit.module('GenerateWithRelativeTime');
+(function () {
+  /* jshint undef: true, unused: true */
+  /* globals QUnit, test, Rx */
 
-var Observable = Rx.Observable,
-    TestScheduler = Rx.TestScheduler,
-    onNext = Rx.ReactiveTest.onNext,
-    onError = Rx.ReactiveTest.onError,
-    onCompleted = Rx.ReactiveTest.onCompleted,
-    subscribe = Rx.ReactiveTest.subscribe;
+  QUnit.module('generateWithRelativeTime');
 
-test('Generate_TimeSpan_Finite', function () {
-    var results, scheduler;
-    scheduler = new TestScheduler();
-    results = scheduler.startWithCreate(function () {
-        return Rx.Observable.generateWithRelativeTime(0, function (x) {
-            return x <= 3;
-        }, function (x) {
-            return x + 1;
-        }, function (x) {
-            return x;
-        }, function (x) {
-            return x + 1;
-        }, scheduler);
+  var Observable = Rx.Observable,
+      TestScheduler = Rx.TestScheduler,
+      onNext = Rx.ReactiveTest.onNext,
+      onError = Rx.ReactiveTest.onError,
+      onCompleted = Rx.ReactiveTest.onCompleted;
+
+  test('generateWithRelativeTime finite', function () {
+    var scheduler = new TestScheduler();
+
+    var results = scheduler.startScheduler(function () {
+      return Observable.generateWithRelativeTime(0, function (x) {
+        return x <= 3;
+      }, function (x) {
+        return x + 1;
+      }, function (x) {
+        return x;
+      }, function (x) {
+        return x + 1;
+      }, scheduler);
     });
-    results.messages.assertEqual(onNext(202, 0), onNext(204, 1), onNext(207, 2), onNext(211, 3), onCompleted(211));
-});
 
-test('Generate_TimeSpan_Throw_Condition', function () {
-    var ex, results, scheduler;
-    ex = 'ex';
-    scheduler = new TestScheduler();
-    results = scheduler.startWithCreate(function () {
-        return Rx.Observable.generateWithRelativeTime(0, function (x) {
-            throw ex;
-        }, function (x) {
-            return x + 1;
-        }, function (x) {
-            return x;
-        }, function (x) {
-            return x + 1;
-        }, scheduler);
+    results.messages.assertEqual(
+      onNext(202, 0),
+      onNext(204, 1),
+      onNext(207, 2),
+      onNext(211, 3),
+      onCompleted(211));
+  });
+
+  test('generateWithRelativeTime throw condition', function () {
+    var error = new Error();
+
+    var scheduler = new TestScheduler();
+
+    var results = scheduler.startScheduler(function () {
+      return Observable.generateWithRelativeTime(0, function () {
+        throw error;
+      }, function (x) {
+        return x + 1;
+      }, function (x) {
+        return x;
+      }, function (x) {
+        return x + 1;
+      }, scheduler);
     });
-    results.messages.assertEqual(onError(201, ex));
-});
 
-test('Generate_TimeSpan_Throw_ResultSelector', function () {
-    var ex, results, scheduler;
-    ex = 'ex';
-    scheduler = new TestScheduler();
-    results = scheduler.startWithCreate(function () {
-        return Rx.Observable.generateWithRelativeTime(0, function (x) {
+    results.messages.assertEqual(
+      onError(201, error));
+  });
+
+  test('generateWithRelativeTime throw result selector', function () {
+    var error = new Error();
+
+    var scheduler = new TestScheduler();
+
+    var results = scheduler.startScheduler(function () {
+      return Observable.generateWithRelativeTime(0, function () {
+        return true;
+      }, function (x) {
+        return x + 1;
+      }, function () {
+        throw error;
+      }, function (x) {
+        return x + 1;
+      }, scheduler);
+    });
+
+    results.messages.assertEqual(
+      onError(201, error));
+  });
+
+  test('generateWithRelativeTime throw iterate', function () {
+    var error = new Error();
+
+    var scheduler = new TestScheduler();
+
+    var results = scheduler.startScheduler(function () {
+        return Observable.generateWithRelativeTime(0, function () {
             return true;
-        }, function (x) {
-            return x + 1;
-        }, function (x) {
-            throw ex;
-        }, function (x) {
-            return x + 1;
-        }, scheduler);
-    });
-    results.messages.assertEqual(onError(201, ex));
-});
-
-test('Generate_TimeSpan_Throw_Iterate', function () {
-    var ex, results, scheduler;
-    ex = 'ex';
-    scheduler = new TestScheduler();
-    results = scheduler.startWithCreate(function () {
-        return Rx.Observable.generateWithRelativeTime(0, function (x) {
-            return true;
-        }, function (x) {
-            throw ex;
+        }, function () {
+            throw error;
         }, function (x) {
             return x;
         }, function (x) {
             return x + 1;
         }, scheduler);
     });
-    results.messages.assertEqual(onNext(202, 0), onError(202, ex));
-});
 
-test('Generate_TimeSpan_Throw_TimeSelector', function () {
-    var ex, results, scheduler;
-    ex = 'ex';
-    scheduler = new TestScheduler();
-    results = scheduler.startWithCreate(function () {
-        return Rx.Observable.generateWithRelativeTime(0, function (x) {
-            return true;
-        }, function (x) {
-            return x + 1;
-        }, function (x) {
-            return x;
-        }, function (x) {
-            throw ex;
-        }, scheduler);
+    results.messages.assertEqual(
+      onNext(202, 0),
+      onError(202, error));
+  });
+
+  test('generateWithRelativeTime throw time selector', function () {
+    var error = new Error();
+
+    var scheduler = new TestScheduler();
+
+    var results = scheduler.startScheduler(function () {
+      return Observable.generateWithRelativeTime(0, function () {
+        return true;
+      }, function (x) {
+        return x + 1;
+      }, function (x) {
+        return x;
+      }, function () {
+        throw error;
+      }, scheduler);
     });
-    results.messages.assertEqual(onError(201, ex));
-});
 
-test('Generate_TimeSpan_Dispose', function () {
-    var results, scheduler;
-    scheduler = new TestScheduler();
-    results = scheduler.startWithDispose(function () {
-        return Rx.Observable.generateWithRelativeTime(0, function (x) {
-            return true;
+    results.messages.assertEqual(
+      onError(201, error));
+  });
+
+  test('generateWithRelativeTime dispose', function () {
+    var scheduler = new TestScheduler();
+
+    var results = scheduler.startScheduler(function () {
+        return Observable.generateWithRelativeTime(0, function () {
+          return true;
         }, function (x) {
-            return x + 1;
+          return x + 1;
         }, function (x) {
-            return x;
+          return x;
         }, function (x) {
-            return x + 1;
+          return x + 1;
         }, scheduler);
-    }, 210);
-    results.messages.assertEqual(onNext(202, 0), onNext(204, 1), onNext(207, 2));
-});
+    }, { disposed: 210 });
+
+    results.messages.assertEqual(
+      onNext(202, 0),
+      onNext(204, 1),
+      onNext(207, 2));
+  });
+
+}());
