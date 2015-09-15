68c59d53bdb3bd4920b6c2e03d67b78685fed2aa
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2015-09-14 20:15:51 -0400

    Updating dist

--- dist/rx.all.js
+++ dist/rx.all.js
@@ -1572,9 +1572,6 @@
 
   /**
    *  Creates an observer from a notification callback.
-   *
-   * @static
-   * @memberOf Observer
    * @param {Function} handler Action that handles a notification.
    * @returns The observer object that invokes the specified handler using a notification corresponding to each message it receives.
    */
@@ -5197,11 +5194,61 @@ Rx.Observable.prototype.flatMapLatest = function(selector, resultSelector, thisA
     }, source);
   };
 
+  var TakeObservable = (function(__super__) {
+    inherits(TakeObservable, __super__);
+    
+    function TakeObservable(source, count) {
+      this.source = source;
+      this.takeCount = count;
+      __super__.call(this);
+    }
+    
+    TakeObservable.prototype.subscribeCore = function (o) {
+      return this.source.subscribe(new InnerObserver(o, this.takeCount));
+    };
+    
+    function InnerObserver(o, c) {
+      this.o = o;
+      this.c = c;
+      this.r = c;
+      this.isStopped = false;
+    }
+    InnerObserver.prototype = {
+      onNext: function (x) {
+        if (this.isStopped) { return; }
+        if (this.r-- > 0) {
+          this.o.onNext(x);
+          this.r <= 0 && this.o.onCompleted();
+        }
+      },
+      onError: function (err) {
+        if (!this.isStopped) {
+          this.isStopped = true;
+          this.o.onError(err);
+        }
+      },
+      onCompleted: function () {
+        if (!this.isStopped) {
+          this.isStopped = true;
+          this.o.onCompleted();
+        }
+      },
+      dispose: function () { this.isStopped = true; },
+      fail: function (e) {
+        if (!this.isStopped) {
+          this.isStopped = true;
+          this.o.onError(e);
+          return true;
+        }
+        return false;
+      }
+    };
+    
+    return TakeObservable;
+  }(ObservableBase));  
+  
   /**
    *  Returns a specified number of contiguous elements from the start of an observable sequence, using the specified scheduler for the edge case of take(0).
-   *
-   *  var res = source.take(5);
-   *  var res = source.take(0, Rx.Scheduler.timeout);
    * @param {Number} count The number of elements to return.
    * @param {Scheduler} [scheduler] Scheduler used to produce an OnCompleted message in case <paramref name="count count</paramref> is set to 0.
    * @returns {Observable} An observable sequence that contains the specified number of elements from the start of the input sequence.
@@ -5209,16 +5256,7 @@ Rx.Observable.prototype.flatMapLatest = function(selector, resultSelector, thisA
   observableProto.take = function (count, scheduler) {
     if (count < 0) { throw new ArgumentOutOfRangeError(); }
     if (count === 0) { return observableEmpty(scheduler); }
-    var source = this;
-    return new AnonymousObservable(function (o) {
-      var remaining = count;
-      return source.subscribe(function (x) {
-        if (remaining-- > 0) {
-          o.onNext(x);
-          remaining <= 0 && o.onCompleted();
-        }
-      }, function (e) { o.onError(e); }, function () { o.onCompleted(); });
-    }, source);
+    return new TakeObservable(this, count);
   };
 
   /**
