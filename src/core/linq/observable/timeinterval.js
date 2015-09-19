925b849796ac97939dc05e7383a08412bd92204b
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2015-09-19 03:13:17 -0400

    Perf for timeInterval

--- src/core/linq/observable/timeinterval.js
+++ src/core/linq/observable/timeinterval.js
@@ -1,3 +1,39 @@
+  var TimeIntervalObservable = (function (__super__) {
+    inherits(TimeIntervalObservable, __super__);
+    function TimeIntervalObservable(source, s) {
+      this.source = source;
+      this._s = s;
+      __super__.call(this);
+    }
+
+    TimeIntervalObservable.prototype.subscribeCore = function (o) {
+      return this.source.subscribe(new TimeIntervalObserver(o, this._s));
+    };
+
+    return TimeIntervalObservable;
+  }(ObservableBase));
+
+  var TimeIntervalObserver = (function (__super__) {
+    inherits(TimeIntervalObserver, __super__);
+
+    function TimeIntervalObserver(o, s) {
+      this._o = o;
+      this._s = s;
+      this._l = s.now();
+      __super__.call(this);
+    }
+
+    TimeIntervalObserver.prototype.next = function (x) {
+      var now = this._s.now(), span = now - this._l;
+      this._l = now;
+      this._o.onNext({ value: x, interval: span });
+    };
+    TimeIntervalObserver.prototype.error = function (e) { this._o.onError(e); };
+    TimeIntervalObserver.prototype.completed = function () { this._o.onCompleted(); };
+
+    return TimeIntervalObserver;
+  }(AbstractObserver));
+
   /**
    *  Records the time interval between consecutive values in an observable sequence.
    *
@@ -9,14 +45,6 @@
    * @returns {Observable} An observable sequence with time interval information on values.
    */
   observableProto.timeInterval = function (scheduler) {
-    var source = this;
     isScheduler(scheduler) || (scheduler = defaultScheduler);
-    return observableDefer(function () {
-      var last = scheduler.now();
-      return source.map(function (x) {
-        var now = scheduler.now(), span = now - last;
-        last = now;
-        return { value: x, interval: span };
-      });
-    });
+    return new TimeIntervalObservable(this, scheduler);
   };
