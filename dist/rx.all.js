6610415097b482a46ad57ec61450851277495de7
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2015-09-20 02:57:32 -0400

    Fixing perf of includes

--- dist/rx.all.js
+++ dist/rx.all.js
@@ -5985,6 +5985,57 @@ Rx.Observable.prototype.flatMapLatest = function(selector, resultSelector, thisA
     return new EveryObservable(this, fn);
   };
 
+  var IncludesObservable = (function (__super__) {
+    inherits(IncludesObservable, __super__);
+    function IncludesObservable(source, elem, idx) {
+      var n = +idx || 0;
+      Math.abs(n) === Infinity && (n = 0);
+
+      this.source = source;
+      this._elem = elem;
+      this._n = n;
+      __super__.call(this);
+    }
+
+    IncludesObservable.prototype.subscribeCore = function (o) {
+      if (this._n < 0) {
+        o.onNext(false);
+        o.onCompleted();
+        return disposableEmpty;
+      }
+
+      return this.source.subscribe(new IncludesObserver(o, this._elem, this._n));
+    };
+
+    return IncludesObservable;
+  }(ObservableBase));
+
+  var IncludesObserver = (function (__super__) {
+    inherits(IncludesObserver, __super__);
+    function IncludesObserver(o, elem, n) {
+      this._o = o;
+      this._elem = elem;
+      this._n = n;
+      this._i = 0;
+      __super__.call(this);
+    }
+
+    function comparer(a, b) {
+      return (a === 0 && b === 0) || (a === b || (isNaN(a) && isNaN(b)));
+    }
+
+    IncludesObserver.prototype.next = function (x) {
+      if (this._i++ >= this._n && comparer(x, this._elem)) {
+        this._o.onNext(true);
+        this._o.onCompleted();
+      }
+    };
+    IncludesObserver.prototype.error = function (e) { this._o.onError(e); };
+    IncludesObserver.prototype.completed = function () { this._o.onNext(false); this._o.onCompleted(); };
+
+    return IncludesObserver;
+  }(AbstractObserver));
+
   /**
    * Determines whether an observable sequence includes a specified element with an optional equality comparer.
    * @param searchElement The value to locate in the source sequence.
@@ -5992,39 +6043,7 @@ Rx.Observable.prototype.flatMapLatest = function(selector, resultSelector, thisA
    * @returns {Observable} An observable sequence containing a single element determining whether the source sequence includes an element that has the specified value from the given index.
    */
   observableProto.includes = function (searchElement, fromIndex) {
-    var source = this;
-    function comparer(a, b) {
-      return (a === 0 && b === 0) || (a === b || (isNaN(a) && isNaN(b)));
-    }
-    return new AnonymousObservable(function (o) {
-      var i = 0, n = +fromIndex || 0;
-      Math.abs(n) === Infinity && (n = 0);
-      if (n < 0) {
-        o.onNext(false);
-        o.onCompleted();
-        return disposableEmpty;
-      }
-      return source.subscribe(
-        function (x) {
-          if (i++ >= n && comparer(x, searchElement)) {
-            o.onNext(true);
-            o.onCompleted();
-          }
-        },
-        function (e) { o.onError(e); },
-        function () {
-          o.onNext(false);
-          o.onCompleted();
-        });
-    }, this);
-  };
-
-  /**
-   * @deprecated use #includes instead.
-   */
-  observableProto.contains = function (searchElement, fromIndex) {
-    //deprecate('contains', 'includes');
-    observableProto.includes(searchElement, fromIndex);
+    return new IncludesObservable(this, searchElement, fromIndex);
   };
 
   var CountObservable = (function (__super__) {
