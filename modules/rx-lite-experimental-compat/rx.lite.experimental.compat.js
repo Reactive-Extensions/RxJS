0b5ed6d7cf36b2978ec70a96fd82c7b44804d8e6
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2015-10-13 15:49:45 -0400

    Fixing isIterable checks

--- modules/rx-lite-experimental-compat/rx.lite.experimental.compat.js
+++ modules/rx-lite-experimental-compat/rx.lite.experimental.compat.js
@@ -93,12 +93,12 @@
   var doneEnumerator = Rx.doneEnumerator = { done: true, value: undefined };
 
   var isIterable = Rx.helpers.isIterable = function (o) {
-    return o[$iterator$] !== undefined;
-  }
+    return o && o[$iterator$] !== undefined;
+  };
 
   var isArrayLike = Rx.helpers.isArrayLike = function (o) {
     return o && o.length !== undefined;
-  }
+  };
 
   Rx.helpers.iterator = $iterator$;
 
--- src/core/headers/enumeratorheader.js
+++ src/core/headers/enumeratorheader.js
@@ -9,11 +9,11 @@
   var doneEnumerator = Rx.doneEnumerator = { done: true, value: undefined };
 
   var isIterable = Rx.helpers.isIterable = function (o) {
-    return o[$iterator$] !== undefined;
-  }
+    return o && o[$iterator$] !== undefined;
+  };
 
   var isArrayLike = Rx.helpers.isArrayLike = function (o) {
     return o && o.length !== undefined;
-  }
+  };
 
   Rx.helpers.iterator = $iterator$;
