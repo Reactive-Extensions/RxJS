f1a0c4f2366550873262ba9785f3df906c7b6bf0
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2015-11-11 00:54:08 -0500

    Adding flatMapFirst

--- src/modular/observable/flatmapfirst.js
+++ src/modular/observable/flatmapfirst.js
@@ -3,6 +3,6 @@
 var FlatMapObservable = require('./flatmapobservable');
 var switchFirst = require('./switchfirst');
 
-module.exports = function flatMapFirst(selector, resultSelector, thisArg) {
-  return switchFirst(new FlatMapObservable(this, selector, resultSelector, thisArg));
+module.exports = function flatMapFirst(source, selector, resultSelector, thisArg) {
+  return switchFirst(new FlatMapObservable(source, selector, resultSelector, thisArg));
 };
