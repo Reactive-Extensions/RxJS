5393ca9b1df87e81b5f39c0ec5786cc93589e0ca
Jan Alonzo <jmalonzo@taguchimail.com>
2016-05-31 09:28:42 +1000

    Add scheduler param of fromPromise to typings. Fixes #1182

--- ts/core/linq/observable/frompromise.ts
+++ ts/core/linq/observable/frompromise.ts
@@ -1,12 +1,14 @@
 /// <reference path="../../observable.ts" />
+/// <reference path="../../concurrency/scheduler.ts" />
 module Rx {
     export interface ObservableStatic {
         /**
         * Converts a Promise to an Observable sequence
         * @param {Promise} An ES6 Compliant promise.
+        * @param {Scheduler} [scheduler] Scheduler to run the conversion from Promises to Observable on. Defaults to Scheduler.async if not provided.
         * @returns {Observable} An Observable sequence which wraps the existing promise success and failure.
         */
- 		fromPromise<T>(promise: Promise<T>): Observable<T>;
+        fromPromise<T>(promise: Promise<T>, scheduler?: IScheduler): Observable<T>;
     }
 }
 
